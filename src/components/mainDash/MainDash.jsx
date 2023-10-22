28916542e4da3fae85fc24dd737dcf7519a67151
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-22 20:10:22 +0500

    table added

--- src/components/mainDash/MainDash.jsx
+++ src/components/mainDash/MainDash.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './MainDash.css'
 import Cards from '../cards/Cards'
+import Table from '../table/Table'
+
 
 
 const MainDash = () => {
@@ -8,6 +10,8 @@ const MainDash = () => {
     <div className='MainDash'>
       <h1>Dashboard</h1>
       <Cards/>
+      <Table/>
+
     </div>
   )
 }
