498f3bede58b0472a947fb739583f437532b79e8
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-22 19:35:33 +0500

    commited

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import MainDash from './components/mainDash/MainDash';
+import Sidebar from './components/sidebar/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="AppGlass">
+          <Sidebar/>
+          <MainDash/>
+          <div></div>
+        </div>
+        
+    </div>
+  );
+}
+
+export default App;
